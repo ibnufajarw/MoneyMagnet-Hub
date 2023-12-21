157c8230da6ba8406b5b9853ce831f0af4ae0927
ImadaTe <tetikus48@gmail.com>
2023-12-22 02:13:42 +0700

    get/post add

---
+++ routes/addStock.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const addController = require("../controllers/addController");
+const router = express.Router();
+
+    router.get("/", addController.getAdd);
+    router.post("/", addController.postAdd);
+
+module.exports = router
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,7 +7,5 @@ const router = express.Router();
 
 router.get("/", HomeController.getAllStocks);
 // router.get("/stocks/:id");
-// router.get("/stocks/add");
-// router.post("/stocks/add");
 
 module.exports = router;
