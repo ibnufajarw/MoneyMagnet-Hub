adb7d00265be8c8c2f9688531aeb309043643529
ibnufajarw <ibnufajarw16@gmail.com>
2023-12-21 19:31:39 +0700

    index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+/** @format */
+
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const path = require("path");
+const routes = require("./routes/index");
+
+const app = express();
+
+app.use(
+	session({
+		secret: "secret-key",
+		resave: false,
+		saveUninitialized: true,
+	})
+);
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/", routes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
