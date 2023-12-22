9606b60db06fdcd52eea94aecba297fce40b277b
ImadaTe <tetikus48@gmail.com>
2023-12-22 08:46:57 +0700

    finish

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,20 +1,38 @@
 /** @format */
-
+const { Op } = require("sequelize");
 const { Stock, Transaction } = require("../models");
 
 class homeController {
 	static async getAllStocks(req, res) {
-		try {
-			const stocks = await Stock.findAll();
-			const topThree = await Stock.findAll({
-				order: [["currentPrice", "DESC"]],
-				limit: 3,
-			});
-			res.render("home", { stocks, topThree });
-		} catch (error) {
-			res.status(500).render({ error: "Internal Server Error" });
-		}
-	}
+        try {
+            let stocks;
+
+            if (req.query.q) {
+                const searchQuery = req.query.q.trim();
+                stocks = await Stock.findAll({
+                    where: {
+                        [Op.or]: [
+                            {
+                                name: { [Op.iLike]: `%${searchQuery}%` }
+                            },
+                        ],
+                    },
+                });
+            } else {
+                stocks = await Stock.findAll();
+            }
+
+            const topThree = await Stock.findAll({
+                order: [["currentPrice", "DESC"]],
+                limit: 3,
+            });
+
+            res.render("home", { stocks, topThree, searchQuery: req.query.q });
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).render("error", { error: "Internal Server Error" });
+        }
+    }
 	static async detail(req, res) {
 		const { id } = req.params.id;
 		try {
@@ -23,7 +41,6 @@ class homeController {
 					model: Transaction,
 				},
 			});
-			// console.log(stocks);
 			res.render("stockDetail", { stocks });
 		} catch (error) {
 			res.status(500).render({ error: "Internal Server Error" });
